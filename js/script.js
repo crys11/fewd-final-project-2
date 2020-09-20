fde33875423d259f924becdc5fc9f30f48c6a888
Crystal <63870889+crys11@users.noreply.github.com>
2020-09-20 16:47:15 -0400

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,16 @@
+console.log("JS Loaded!");
+
+const name = "Marc";
+
+console.log(name);
+
+// const clickMe = document.getElementById('fake-search-bar');
+// clickMe.addEventListener('click', function() {
+// console.log("clicked");
+
+// const hiddenMenu = document.getElementById('fake-nav-area');
+// hiddenMenu.addEventListener('mouseover', function() {
+// console.log("hover");
+
+// function showMenu(){
+//   document.getElementById('nav-container').classList.add('show');
